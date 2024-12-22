74658142fb949c2afa9b84426938f947bf4263b6
Kinfe123 <kinfishtech@gmail.com>
2024-12-22 11:02:54 +0300

    fix: star on corner

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
@@ -16,8 +16,8 @@ const Approach = () => {
         <Card
           title="Planning & Strategy"
           icon={<AceternityIcon order="Phase 1" />}
-          des="We'll collaborate to map out your website's goals, target audience, 
-          and key functionalities. We'll discuss things like site structure, 
+          des="We'll collaborate to map out your website's goals, target audience,
+          and key functionalities. We'll discuss things like site structure,
           navigation, and content requirements."
         >
           <CanvasRevealEffect
@@ -51,7 +51,7 @@ const Approach = () => {
         <Card
           title="Development & Launch"
           icon={<AceternityIcon order="Phase 3" />}
-          des="This is where the magic happens! Based on the approved design, 
+          des="This is where the magic happens! Based on the approved design,
           I'll translate everything into functional code, building your website
           from the ground up."
         >
@@ -87,20 +87,18 @@ const Card = ({
       onMouseLeave={() => setHovered(false)}
       // change h-[30rem] to h-[35rem], add rounded-3xl
       className="border border-black/[0.2] group/canvas-card flex items-center justify-center
-       dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative lg:h-[35rem] rounded-3xl "
+       dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative lg:h-[35rem] rounded-none "
       style={{
-        //   add these two
-        //   you can generate the color from here https://cssgradient.io/
         background: "rgb(4,7,29)",
         backgroundColor:
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
       {/* change to h-10 w-10 , add opacity-30  */}
-      <Icon className="absolute h-10 w-10 -top-3 -left-3 dark:text-white text-black opacity-30" />
-      <Icon className="absolute h-10 w-10 -bottom-3 -left-3 dark:text-white text-black opacity-30" />
-      <Icon className="absolute h-10 w-10 -top-3 -right-3 dark:text-white text-black opacity-30" />
-      <Icon className="absolute h-10 w-10 -bottom-3 -right-3 dark:text-white text-black opacity-30" />
+      <Icon className="absolute h-10 w-10 -top-5 -left-5 dark:text-white text-black opacity-30" />
+      <Icon className="absolute h-10 w-10 -bottom-5 -left-5 dark:text-white text-black opacity-30" />
+      <Icon className="absolute h-10 w-10 -top-5 -right-5 dark:text-white text-black opacity-30" />
+      <Icon className="absolute h-10 w-10 -bottom-5 -right-5 dark:text-white text-black opacity-30" />
 
       <AnimatePresence>
         {hovered && (
@@ -118,7 +116,7 @@ const Card = ({
         <div
           // add this for making it center
           // absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]
-          className="text-center group-hover/canvas-card:-translate-y-4 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] 
+          className="text-center group-hover/canvas-card:-translate-y-4 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]
         group-hover/canvas-card:opacity-0 transition duration-200 min-w-40 mx-auto flex items-center justify-center"
         >
           {icon}
@@ -126,7 +124,7 @@ const Card = ({
         <h2
           // change text-3xl, add text-center
           className="dark:text-white text-center text-3xl opacity-0 group-hover/canvas-card:opacity-100
-         relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white 
+         relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white
          group-hover/canvas-card:-translate-y-2 transition duration-200"
         >
           {title}
@@ -158,7 +156,7 @@ const AceternityIcon = ({ order }: { order: string }) => {
          bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"
         />
         <span
-          className="inline-flex h-full w-full cursor-pointer items-center 
+          className="inline-flex h-full w-full cursor-pointer items-center
         justify-center rounded-full bg-slate-950 px-5 py-2 text-purple backdrop-blur-3xl font-bold text-2xl"
         >
           {order}
